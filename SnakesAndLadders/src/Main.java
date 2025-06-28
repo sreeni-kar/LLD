fefe4265170a76a46138891997bf6f3e53a47a3a
Sreenivas Karthik <bandisreenivakarthik@gmail.com>
2025-06-28 15:39:43 +0530

    Snakes and Ladders LLD .

--- SnakesAndLadders/src/Main.java
+++ SnakesAndLadders/src/Main.java
@@ -1,5 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 class Main{
     public static void main(String[] args){
-        System.out.println("testing");
+        int boardSize = 50;
+
+        HashMap<Integer,Integer> snakes = new HashMap<>();
+        snakes.put(35,22);
+        snakes.put(47,17);
+        snakes.put(26,7);
+
+        HashMap<Integer,Integer> ladders = new HashMap<>();
+        ladders.put(10,30);
+        ladders.put(27,44);
+
+        int numberOfDice = 1;
+
+        List<Player> playerList = new ArrayList<>();
+        playerList.add(new Player(1,"Jon",0));
+        playerList.add(new Player(2,"Mark", 0));
+        playerList.add(new Player(3, "Daniel", 0));
+
+        Board board = new Board(boardSize,snakes,ladders,numberOfDice,playerList);
+
+        board.startGame();
     }
 }
